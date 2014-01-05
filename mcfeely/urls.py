2b66eb4aeaf8c8f3f122ed6624ee253cff66309c
Richard Cox <rick@bot37.com>
2014-01-04 22:54:49 -0700

    django 1.3 bugfix

--- mcfeely/urls.py
+++ mcfeely/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, url
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
+    
 from mcfeely.views import UnsubscribeView
 
 urlpatterns = patterns('mcfeely.views',
