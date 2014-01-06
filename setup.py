39d92d03d0f5f26ebe8ed695896b892f78193237
Richard Cox <rick@bot37.com>
2014-01-05 21:31:53 -0700

    pypi version bump

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class RunserverCommand(Command):
 
 setup(
     name='django-mcfeely',
-    version='0.1',
+    version='0.2',
     description='Email queuing system for django',
     author='Richard Cox',
     author_email='code@bot37.com',
